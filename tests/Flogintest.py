7c17b334381710107e6cf540131f27dc4192f06b
Nahed Al Awlaki <Nahed.atik@hotmail.com>
2023-05-01 18:48:23 +0200

    fixing the integration testing

--- tests/Flogintest.py
+++ tests/Flogintest.py
@@ -13,30 +13,27 @@ from login import Ui_Form
 
 def Ui_test():
     app = QApplication(sys.argv)
-    Ui_test = Ui_Form()
-    Ui_test.setupUi(Ui_test)
-    yield Ui_test
+    ui_form = Ui_Form()
+    ui_form.setupUi(ui_form)
+    yield ui_form
     app.quit()
     
     
-def test_login_button(Ui_test):
-    assert Ui_test.b1.text() == "Login"
-    QTest.mouseClick(Ui_test.b1, Qt.LeftButton)
+def test_loginButton(ui_form):
+    assert ui_form.b1.text() == "Login"
+    QTest.mouseClick(ui_form.b1, Qt.LeftButton)
     
-def test_signUp_button(Ui_test):
-    assert Ui_test.b2.text() == "Sign up"
-    QTest.mouseClick(Ui_test.b2, Qt.LeftButton)
+def test_signUpButton(ui_form):
+    assert ui_form.b2.text() == "Sign up"
+    QTest.mouseClick(ui_form.b2, Qt.LeftButton)
     
-def test_guest_button(Ui_test):
-    assert Ui_test.b5.text() == "Continue as a Guest"
-    QTest.mouseClick(Ui_test.b5, Qt.LeftButton)
+def test_guestButton(ui_form):
+    assert ui_form.b5.text() == "Continue as a Guest"
+    QTest.mouseClick(ui_form.b5, Qt.LeftButton)
 
- 
-def text_box_test(Ui_test):
-    Ui_test.tb1.setText("Username")
-    Ui_test.lineEdit.setText("Password")
-    assert Ui_test.tb1.text() == "Username"
-    assert Ui_test.lineEdit.text() == "Password"
 
-    
-    
+def test_textBox(ui_form):
+    ui_form.tb1.setText("Username")
+    ui_form.lineEdit.setText("Password")
+    assert ui_form.tb1.text() == "Username"
+    assert ui_form.lineEdit.text() == "Password"
