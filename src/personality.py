90cd81befd249097395da5414fb41594cf7c8815
lakshmi vishal <lakshmivishal9496@gmail.com>
2023-05-02 13:28:27 +0200

    Added personality type

--- src/personality.py
+++ src/personality.py
@@ -17,26 +17,7 @@ personality_list = {
     "ENTJ": "Extraverted, Intuitive, Thinking, Judging"
 }
 
-personality_recomendation = {
-    "ISTJ": "Practice adaptability,\
-    embrace change, and explore creative problem-solving. \
-    Make time for leisure and social activities\
-    to balance work and personal life.\
-    (source: Costa Jr, P. T., & McCrae, R. R. (1992).\
-    Revised NEO Personality Inventory (NEO-PI-R) and\
-    NEO Five-Factor Inventory (NEO-FFI) manual.\
-    Psychological Assessment Resources.)",
-    
-    "ISFJ": "Develop assertiveness, set boundaries, and\
-    engage in self-care practices.\
-    Explore new experiences and ideas to expand perspectives.\
-    (source: Judge, T. A., & Bono, J. E. (2001).\
-    Relationship of core self-evaluations traits--self-esteem,\
-    generalized self-efficacy, locus of control, and emotional stability\
-    --with job satisfaction and job performance: A meta-analysis.\
-    Journal of Applied Psychology, 86(1), 80-92.)",
 
-}
 personality_recommendation = {
  "ISTJ": "Practice adaptability,\
     embrace change, and explore creative problem-solving. \
