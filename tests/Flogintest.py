6255c958a7bfa39b28b5edeab3e5bf28c09507b0
Nahed Al Awlaki <Nahed.atik@hotmail.com>
2023-05-02 13:28:40 +0200

    Functional feature testing

--- tests/Flogintest.py
+++ tests/Flogintest.py
@@ -2,38 +2,38 @@ import sys
 import os
 src_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "src"))
 sys.path.append(src_path)
-
-from PyQt5.QtCore import Qt
 import pytest
+from PyQt5.QtCore import Qt
+from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtTest import QTest
 from login import Ui_Form
 
-
-
-def Ui_test():
+@pytest.fixture
+def ui_form():
     app = QApplication(sys.argv)
-    ui_form = Ui_Form()
-    ui_form.setupUi(ui_form)
-    yield ui_form
+    ui = QtWidgets.QWidget()
+    form = Ui_Form()
+    form.setupUi(ui)
+    ui.show()
+    yield form
     app.quit()
-    
-    
-def test_loginButton(ui_form):
-    assert ui_form.b1.text() == "Login"
-    QTest.mouseClick(ui_form.b1, Qt.LeftButton)
-    
-def test_signUpButton(ui_form):
-    assert ui_form.b2.text() == "Sign up"
-    QTest.mouseClick(ui_form.b2, Qt.LeftButton)
-    
-def test_guestButton(ui_form):
-    assert ui_form.b5.text() == "Continue as a Guest"
-    QTest.mouseClick(ui_form.b5, Qt.LeftButton)
 
+class TestLoginUI:
+    def test_loginButton(self, ui_form):
+        assert ui_form.b1.text() == "Login"
+        QTest.mouseClick(ui_form.b1, Qt.LeftButton)
+    
+    def test_signUpButton(self, ui_form):
+        assert ui_form.b2.text() == "Sign Up"
+        QTest.mouseClick(ui_form.b2, Qt.LeftButton)
+    
+    def test_guestButton(self, ui_form):
+        assert ui_form.b5.text() == "Continue as a Guest"
+        QTest.mouseClick(ui_form.b5, Qt.LeftButton)
 
-def test_textBox(ui_form):
-    ui_form.tb1.setText("Username")
-    ui_form.lineEdit.setText("Password")
-    assert ui_form.tb1.text() == "Username"
-    assert ui_form.lineEdit.text() == "Password"
+    def test_textBox(self, ui_form):
+        ui_form.tb1.setText("Username")
+        ui_form.lineEdit.setText("Password")
+        assert ui_form.tb1.text() == "Username"
+        assert ui_form.lineEdit.text() == "Password"
